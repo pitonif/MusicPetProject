8d910d15710befceccec37bc222e0dae3a84c8e4
Lobachkov Oleg <olobachkov@gmail.com>
2022-06-22 15:22:24 +0300

    create my pet

---
+++ MusicPet/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  MusicPet
+//
+//  Created by Oleg on 21.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+        
+        
+    }
+
+
+}
+
