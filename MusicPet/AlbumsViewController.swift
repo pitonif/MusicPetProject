e9b56212db2ac56e518a9b7e619e0eb944765b0a
Lobachkov Oleg <olobachkov@gmail.com>
2022-06-24 17:39:33 +0300

    add full AlbumsViewController

--- MusicPet/AlbumsViewController.swift
+++ MusicPet/AlbumsViewController.swift
@@ -25,6 +25,10 @@ class AlbumsViewController: UIViewController {
         setupViews()
         setupDelegate()
         setupNavigationBar()
+        setupConstraints()
+        setupSearchBar()
+        
+       
     }
     
     private func setupViews() {
@@ -44,7 +48,7 @@ class AlbumsViewController: UIViewController {
         
         navigationItem.searchController = searchController
         
-        let userInfoButton = createCustomButton(selector: #selector(<#T##@objc method#>))
+        let userInfoButton = createCustomButton(selector: #selector(userInfoButtonTapped))
         navigationItem.rightBarButtonItem = userInfoButton
     }
     
@@ -52,6 +56,60 @@ class AlbumsViewController: UIViewController {
         searchController.searchBar.placeholder = "Search"
         searchController.obscuresBackgroundDuringPresentation = true
     }
-   
-    @objc func 
+    
+    @objc func userInfoButtonTapped() {
+        let userInfoVC = UserInfoViewController()
+        navigationController?.pushViewController(userInfoVC, animated: true)
+    }
+    
+    
+}
+
+
+//MARK: - UISearchBarDelegate
+
+extension AlbumsViewController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
+    }
+}
+
+//MARK: - UITableViewDelegate and UITableViewDataSource
+
+extension AlbumsViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! AlbumsTableViewCell
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let datailVC = DetailAlbumViewController()
+        navigationController?.pushViewController(datailVC, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+           70
+       }
+}
+
+//MARK: - SetConstraints
+extension AlbumsViewController {
+    
+    private func setupConstraints() {
+        
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            tableView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
+        ])
+    }
 }
---
+++ MusicPet/DetailAlbumViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DetailAlbumViewController.swift
+//  MusicPet
+//
+//  Created by Oleg on 24.06.2022.
+//
+
+import UIKit
+
+class DetailAlbumViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    
+
+}
