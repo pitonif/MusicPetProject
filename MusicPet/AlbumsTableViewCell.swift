89c1fa96c22b75908814368d650808cf13e8ed55
Lobachkov Oleg <olobachkov@gmail.com>
2022-06-27 13:50:11 +0300

    finish albumTableViewCell design

--- MusicPet/AlbumsTableViewCell.swift
+++ MusicPet/AlbumsTableViewCell.swift
@@ -19,20 +19,86 @@ class AlbumsTableViewCell: UITableViewCell {
     
     private let albumNameLabel: UILabel = {
         let label = UILabel()
+        label.text = "Album name"
         label.font = UIFont.systemFont(ofSize: 22)
         label.tintColor = .black
-        label.text = "Album name"
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
+    
+    private let artistNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Artist name"
+        label.font = UIFont.systemFont(ofSize: 15)
+        label.tintColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let countTrackLabel: UILabel = {
+        let label = UILabel()
+        label.text = "16 tracks"
+        label.font = UIFont.systemFont(ofSize: 15)
+        label.tintColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    var stackView = UIStackView()
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
+    override func layoutSubviews() {
+            super.layoutSubviews()
+            
+        albumLogoImage.layer.cornerRadius = 10
+        }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+            super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        setupViews()
+        setupConstraints()
+        }
+    
+    required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+    
+    private func setupViews() {
+        self.backgroundColor = .clear
+        self.selectionStyle = .none
+        
+        self.addSubview(albumLogoImage)
+        self.addSubview(albumNameLabel)
+        
+        stackView = UIStackView(arrangedSubviews: [artistNameLabel,
+                                                   countTrackLabel],
+                                axis: .horizontal,
+                                spacing: 10,
+                                distribution: .equalCentering)
+        
+        self.addSubview(stackView)
+        
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
+    
+    private func setupConstraints() {
+       
+              NSLayoutConstraint.activate([
+                albumLogoImage.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+                albumLogoImage.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 15),
+                albumLogoImage.heightAnchor.constraint(equalToConstant: 60),
+                albumLogoImage.widthAnchor.constraint(equalToConstant: 60)
+               ])
+               
+               NSLayoutConstraint.activate([
+                   albumNameLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 10),
+                   albumNameLabel.leadingAnchor.constraint(equalTo: albumLogoImage.trailingAnchor, constant: 10),
+                   albumNameLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10)
+               ])
+               
+               NSLayoutConstraint.activate([
+                   stackView.topAnchor.constraint(equalTo: albumNameLabel.bottomAnchor, constant: 10),
+                   stackView.leadingAnchor.constraint(equalTo: albumLogoImage.trailingAnchor, constant: 10),
+                   stackView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10)
+               ])
     }
-
 }
