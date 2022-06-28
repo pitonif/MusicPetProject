cad03965c4e83e69f7dba2db7522595a3e09015c
Lobachkov Oleg <olobachkov@gmail.com>
2022-06-28 14:37:59 +0300

    add all UIElements in DetailAlbumVC and add SongsCollectionViewVC

--- MusicPet/DetailAlbumViewController.swift
+++ MusicPet/DetailAlbumViewController.swift
@@ -18,7 +18,9 @@ class DetailAlbumViewController: UIViewController {
     
     private let albumNameLabel: UILabel = {
         let label = UILabel()
+        label.numberOfLines = 0
         label.text = "Album name"
+        label.font = UIFont.systemFont(ofSize: 20)
         label.tintColor = .black
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
@@ -32,13 +34,34 @@ class DetailAlbumViewController: UIViewController {
         return label
     }()
     
-    private let releseDataLabel: UILabel = {
+    private let releaseDataLabel: UILabel = {
         let label = UILabel()
-        label.text = "Relese Date"
+        label.text = "Release Date"
         label.tintColor = .black
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
+    
+    private let trackCountLabel: UILabel = {
+        let label = UILabel()
+        label.text = "10 tracks"
+        label.tintColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = 5
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .white
+        collectionView.bounces = false
+        collectionView.register(SongsCollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+    
+    private var stackView = UIStackView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ MusicPet/SongsCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  SongsCollectionViewCell.swift
+//  MusicPet
+//
+//  Created by Oleg on 28.06.2022.
+//
+
+import UIKit
+
+class SongsCollectionViewCell: UICollectionViewCell {
+    
+}
