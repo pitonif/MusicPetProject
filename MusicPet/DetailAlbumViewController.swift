c33209075b1ec6f1cfc74fb1b33a5e2cddce26c1
Lobachkov Oleg <olobachkov@gmail.com>
2022-06-28 14:24:33 +0300

    work with detailAlbumVC

--- MusicPet/DetailAlbumViewController.swift
+++ MusicPet/DetailAlbumViewController.swift
@@ -8,6 +8,37 @@
 import UIKit
 
 class DetailAlbumViewController: UIViewController {
+    
+    private let albumLogoView: UIImageView = {
+        let image = UIImageView()
+        image.backgroundColor = .red
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
+    private let albumNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Album name"
+        label.tintColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let artistNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Artist name"
+        label.tintColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let releseDataLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Relese Date"
+        label.tintColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
